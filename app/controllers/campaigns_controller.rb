ef774be415ba66721774b65b1e4db4b9781646d7
Louis Brutton <louisbrutton@gmail.com>
2016-07-12 15:56:43 +0000

    add all missing financials

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -9,13 +9,6 @@ class CampaignsController < ApplicationController
       @end_date = Date.parse(params[:end_date])
     end
     
-    Campaign.all.each do |campaign|
-      campaign.cpc = 0.2 #this is hard coded for now - need to allow this value to be changed from the admin
-      campaign.spend = (campaign.clicks * 0.2).round(2)
-      campaign.ecpi = ((campaign.clicks * 0.2)/campaign.installs).round(2)
-      campaign.save
-    end
-    
   end
 
 end
