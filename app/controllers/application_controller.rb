5fece724315baa606a0472293e809c5bfcb22de5
David Granados <daviliano2@gmail.com>
2016-07-13 12:21:43 +0200

    adding localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def authenticate_any!
     if admin_signed_in?
       true
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  devise_for :admins, controllers: { sessions: 'admins/sessions' }
-  post 'campaigns/update'
-  post 'campaigns/show'
-  post 'placements/show'
-  get 'placements/show'
+  scope "(:locale)", locale: /en|jp/ do
+    devise_for :users, controllers: { sessions: 'users/sessions' }
+    devise_for :admins, controllers: { sessions: 'admins/sessions' }
+    post 'campaigns/update'
+    post 'campaigns/show'
+    post 'placements/show'
+    get 'placements/show'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
+  end
+  get '/:locale' => 'campaigns#show'
   # You can have the root of your site routed with "root"
   root 'campaigns#show'
 
